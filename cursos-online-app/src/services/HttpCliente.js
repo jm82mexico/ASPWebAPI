7d3a6bb01967eca96adb77e6fd821fc7657f379f
jm82mexico <jmdelan2012@gmail.com>
2022-10-04 17:10:03 -0500

    Configuración de token
    Configuración del token de seguridad en todas las peticiones

--- cursos-online-app/src/services/HttpCliente.js
+++ cursos-online-app/src/services/HttpCliente.js
@@ -2,6 +2,19 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:5227/api'
 
+axios.interceptors.request.use(
+  (config) => {
+    const token_seguridad = window.localStorage.getItem('token_seguridad')
+    if (token_seguridad) {
+      config.headers.Authorization = 'Bearer ' + token_seguridad
+      return config
+    }
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
 const requestGenerico = {
   get: (url) => axios.get(url),
   post: (url, body) => axios.post(url, body),
