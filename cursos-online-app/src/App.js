c777f6850d5c26b345dd91751d8b929eca98bdd2
jm82mexico <jmdelan2012@gmail.com>
2022-10-05 08:31:16 -0500

    Rutas básicas en App

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -1,14 +1,28 @@
-import { ThemeProvider } from '@mui/material'
+import { Grid, ThemeProvider } from '@mui/material'
 import theme from './theme/theme'
 import React from 'react'
-// import RegistrarUsuario from './Components/Seguridad/RegistrarUsuario'
+import RegistrarUsuario from './Components/Seguridad/RegistrarUsuario'
 import Login from './Components/Seguridad/Login'
-// import PerfilUsuario from './Components/Seguridad/PerfilUsuario'
+import PerfilUsuario from './Components/Seguridad/PerfilUsuario'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Login />
+      <Router>
+        <Grid container>
+          <Routes>
+            <Route exact path="/auth/login" element={<Login />} />
+            <Route
+              exact
+              path="/auth/registrar"
+              element={<RegistrarUsuario />}
+            />
+            <Route exact path="/auth/perfil" element={<PerfilUsuario />} />
+            <Route exact path="/" element={<PerfilUsuario />} />
+          </Routes>
+        </Grid>
+      </Router>
     </ThemeProvider>
   )
 }
